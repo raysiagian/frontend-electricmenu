0baccb04577864fa973d42885b944a684422d830
raysiagian <anggasiagian17@gmail.com>
2026-05-29 12:44:02 +0700

    fix: update BASE URL

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,9 @@ import axios from "axios";
 // const BASE_URL = "http://localhost:3000/api"
 const ENV_URL = import.meta.env.VITE_BASE_URL || "https://backend-electricmenu.onrender.com";
 
+// Gabungkan langsung dengan /api
+export const BASE_URL = `${ENV_URL.replace(/\/$/, "")}/api`;
+
 const api = axios.create({
     baseURL: BASE_URL,
 });
--- src/services/public-api.js
+++ src/services/public-api.js
@@ -2,7 +2,11 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:3000/api"
-export const BASE_URL = process.env.BASE_URL
+const ENV_URL = import.meta.env.VITE_BASE_URL || "https://backend-electricmenu.onrender.com";
+
+// Gabungkan langsung dengan /api
+export const BASE_URL = `${ENV_URL.replace(/\/$/, "")}/api`;
+
 
 const publicApi = axios.create({
     baseURL: BASE_URL,

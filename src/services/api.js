b260030378e2bbbf6bad0abf9527181dc1cbbc75
raysiagian <anggasiagian17@gmail.com>
2026-05-29 12:01:45 +0700

    changing api base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api"
+// const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://backend-electricmenu.onrender.com/api"
 
 const api = axios.create({
     baseURL: BASE_URL,
--- src/services/public-api.js
+++ src/services/public-api.js
@@ -1,7 +1,8 @@
 // public-api.js - untuk route public, tanpa token
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api"
+// const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://backend-electricmenu.onrender.com/api"
 
 const publicApi = axios.create({
     baseURL: BASE_URL,
